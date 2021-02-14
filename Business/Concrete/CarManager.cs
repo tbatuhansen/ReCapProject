70e8663f9580e8927e95ebe8e39e8d5383affbb6
tbatuhansen <47259557+tbatuhansen@users.noreply.github.com>
2021-02-14 18:28:43 +0300

    Entity Framework dahil edildi.

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -16,9 +16,41 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        public void Add(Car car)
+        {
+            if(car.Description.Length>2&&car.DailyPrice>0)
+            {
+                _carDal.Add(car);
+                Console.WriteLine("Araç başarıyla eklendi.");
+            }
+            else
+                Console.WriteLine("Ekleme işlemi başarısız ! ");
+        }
+
+        public void Delete(Car car)
+        {
+            _carDal.Delete(car);
+            Console.WriteLine("Araç silindi ! ");
+        }
+
         public List<Car> GetAll()
         {
             return _carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int brandId)
+        {
+            return _carDal.GetAll(p => p.BrandId == brandId);
+        }
+
+        public List<Car> GetCarsByColorId(int colorId)
+        {
+            return _carDal.GetAll(p => p.ColorId == colorId);
+        }
+
+        public void Update(Car car)
+        {
+            _carDal.Update(car);
         }
     }
 }
