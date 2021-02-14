b4923ac3e9d04b803d31578b56db06f8094caad8
tbatuhansen <47259557+tbatuhansen@users.noreply.github.com>
2021-02-14 19:13:48 +0300

    Deneme

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete;
 using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
 using System;
 
 namespace ConsoleUI
@@ -10,6 +11,17 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             CarManager carManager = new CarManager(new EfCarDal());
+            Car car = new Car
+            {
+                Id = 7,
+                BrandId = 1,
+                ColorId = 3,
+                ModelYear = "2016",
+                DailyPrice = 250,
+                Description = "Volkswagen Golf 1.6 TDI Highline DSG"
+            };
+
+            carManager.Add(car);
             foreach (var item in carManager.GetAll())
             {
                 Console.WriteLine(item.ModelYear + " " + item.Description);
